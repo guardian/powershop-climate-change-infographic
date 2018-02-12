c65b0a804f4737542a9757bf5cc9817c38828f98
Indrek Paas <indrekpaas@users.noreply.github.com>
2018-02-12 11:56:34 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Australian Ethical – Invest for a Better World
+
+## Development
+
+```
+npm install
+npm start
+```
+
+## Staging
+
+```
+npm run stage
+```
+
+## Production
+
+### Build locally
+
+```
+npm run build
+```
+
+### Deploy to CDN
+
+```
+npm run deploy
+```
---
+++ gulpfile.js
@@ -0,0 +1,148 @@
+const config = require('./config.json')
+
+const autoprefixer = require('gulp-autoprefixer')
+const browserSync = require('browser-sync')
+const concat = require('gulp-concat')
+const del = require('del')
+const file = require('gulp-file')
+const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
+const npmSass = require('npm-sass')
+const nunjucks = require('gulp-nunjucks')
+const plumber = require('gulp-plumber')
+const removeCode = require('gulp-remove-code')
+const rename = require('gulp-rename')
+const runSequence = require('run-sequence')
+const s3Upload = require('gulp-s3-upload')
+const sass = require('gulp-sass')
+const sassVars = require('gulp-sass-vars')
+const sourcemaps = require('gulp-sourcemaps')
+const surge = require('gulp-surge')
+const uglify = require('gulp-uglify')
+
+let domain = path = project = version = cdn = null
+
+if (process.env.NODE_ENV === 'production') {
+  domain = 'https://interactive.guim.co.uk'
+  path = 'atoms'
+  project = config.path
+  version = `v/${Date.now()}`
+  cdn = `${domain}/${path}/${project}/${version}/`
+}
+
+gulp.task('browser-sync', () =>
+  browserSync.init({
+    server: {
+      baseDir: 'dest',
+      index: 'main.html'
+    }
+  })
+)
+
+gulp.task('build', callback =>
+  runSequence('clean', ['images', 'scripts', 'stylesheets', 'templates'], callback)
+)
+
+gulp.task('clean', () =>
+  del('dest')
+)
+
+gulp.task('default', ['build', 'watch'])
+
+function s3(cacheControl, keyPrefix) {
+  return s3Upload()({
+    'Bucket': 'gdn-cdn',
+    'ACL': 'public-read',
+    'CacheControl': cacheControl,
+    'keyTransform': fn => `${keyPrefix}/${fn}`
+  })
+}
+
+gulp.task('deploy', ['build'], () =>
+  gulp.src('dest/**/*')
+    .pipe(s3('max-age=31536000', `${path}/${project}/${version}`))
+    .on('end', () =>
+      gulp.src('config.json')
+        .pipe(file('preview', version))
+        .pipe(file('live', version))
+        .pipe(s3('max-age=30', `${path}/${project}`))
+    )
+)
+
+gulp.task('images', () =>
+  gulp.src('src/images/*')
+    .pipe(gulp.dest('dest/images'))
+)
+
+gulp.task('scripts', () =>
+  gulp.src([
+    'node_modules/when-in-viewport/dist/whenInViewport.js',
+    'src/scripts/main.js'
+  ])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+      .pipe(concat('main.js'))
+      .pipe(uglify())
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('dest'))
+    .on('end', browserSync.reload)
+)
+
+gulp.task('stage', ['build'], () =>
+  surge({
+    project: 'dest',
+    domain: 'powershop-climate-change-infographic.surge.sh'
+  })
+)
+
+gulp.task('stylesheets', () => {
+  const variables = {
+    cdn: cdn
+  }
+
+  gulp.src('src/stylesheets/*.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+      .pipe(sassVars(variables, {
+        verbose: true
+      }))
+      .pipe(sass({
+        importer: npmSass.importer,
+        outputStyle: 'compressed',
+        precision: 10
+      }))
+      .pipe(autoprefixer())
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('dest'))
+    .pipe(browserSync.stream({
+      match: '**/*.css'
+    }))
+  }
+)
+
+gulp.task('templates', () =>
+  gulp.src('src/templates/*.njk')
+    .pipe(plumber())
+    .pipe(nunjucks.compile({
+      cdn: cdn
+    }))
+    .pipe(rename({
+      extname: '.html'
+    }))
+    .pipe(removeCode({
+      production: true
+    }))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('dest'))
+    .on('end', browserSync.reload)
+)
+
+gulp.task('watch', ['browser-sync'], () => {
+  gulp.watch('src/images/*', ['images'])
+  gulp.watch('src/scripts/**/*.js', ['scripts'])
+  gulp.watch('src/stylesheets/**/*.scss', ['stylesheets'])
+  gulp.watch('src/templates/**/*.njk', ['templates'])
+})
